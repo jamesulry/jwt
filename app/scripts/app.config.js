07847799c421b7fd5a71d71e6227798e4a7c4eb9
jamesu <james_ulry@ultimatesoftware.com>
2015-03-02 16:29:41 -0500

    initial commit

---
+++ app/scripts/app.config.js
@@ -0,0 +1,20 @@
+﻿'use strict';
+
+angular
+  .module('jwtApp')
+  .config(function ($urlRouterProvider, $stateProvider) {
+    
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+    
+    .state('main', {
+        url: '/',
+        templateUrl: '/views/main.html'
+    })
+    
+    .state('register', {
+        url: '/register',
+        templateUrl: '/views/register.html'
+    });
+});
---
+++ app/scripts/directives/validateEquals.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name jwtApp.directive:validateEquals
+ * @description
+ * # sameAs
+ */
+angular.module('jwtApp')
+  .directive('validateEquals', function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, element, attrs, ngModelCtrl) {
+            function validateEquals(value) {
+                var valid = (value === scope.$eval(attrs.validateEquals));
+                ngModelCtrl.$setValidity('equal', valid);
+                return valid ? value : undefined;
+            }
+            
+            ngModelCtrl.$parsers.push(validateEquals);
+            ngModelCtrl.$formatters.push(validateEquals);
+            
+            scope.$watch(attrs.validateEquals, function () {
+                ngModelCtrl.$setViewValue(ngModelCtrl.$viewValue);
+            });
+        }
+    };
+});
