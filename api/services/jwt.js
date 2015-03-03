4d50577ce1ca62090153343c512a9d640c9c89a7
jamesu <james_ulry@ultimatesoftware.com>
2015-03-03 16:41:57 -0500

    jwt token creation

--- api/services/jwt.js
+++ api/services/jwt.js
@@ -1,3 +1,25 @@
 /**
  * Created by ssd on 3/3/15.
  */
+var crypto = require("crypto");
+
+
+exports.encode = function(payload, secret) {
+    var algorithm = "HS256";
+
+    var header = {
+        type: "jwt",
+        algorithm: algorithm
+    };
+
+    var jwt = base64Encode(JSON.stringify(header)) + "." + base64Encode(JSON.stringify(payload));
+    return jwt + "." + sign(jwt, secret);
+};
+
+function base64Encode(str) {
+    return new Buffer(str).toString("base64");
+}
+
+function sign(str, key){
+    return crypto.createHmac("sha256", key).update(str).digest("base64");
+}
